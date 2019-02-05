74678f0ec14c49920938cc9a0317509d51413254
sheeja22 <45845291+sheeja22@users.noreply.github.com>
2019-02-05 03:42:36 -0600

    Create median.py

---
+++ median.py
@@ -0,0 +1,12 @@
+Input  : a[] = {1, 3, 4, 2, 6, 5, 8, 7}
+Output : Mean = 4.5
+  Median = 4.5
+Sum of the elements is 1 + 3 + 4 + 2 + 6 + 
+5 + 8 + 7 = 36
+Mean = 36/8 = 4.5
+Since number of elements are even, median
+is average of 4th and 5th largest elements.
+which means (4 + 5)/2 = 4.
+Input  : a[] = {4, 4, 4, 4, 4}
+Output : Mean = 4
+         Median = 4 
